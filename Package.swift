c314383925d880299f596e2313cea5dd796993a0
Chris Spadling <cjspradling@gmail.com>
2024-12-19 22:36:15 -0800

    typo

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Holocron",
     platforms: [
-        .iOS(v18),
+        .iOS(.v18),
     ]
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
